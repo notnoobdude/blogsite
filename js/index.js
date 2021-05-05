da66144841c3d9e2a18ffdd96cd21e39b13c9c48
Dan Fabro <timpaxerror@gmail.com>
2021-05-06 01:13:04 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
-'use strict';
-
 /*
  * Handles mobile nav
  */
 
 function toggleMobileNavState() {
-  var body = document.querySelector('body');
-  body.classList.toggle('nav--active');
+  const body = document.querySelector('body')
+  body.classList.toggle('nav--active')
 }
 
 /*
@@ -14,8 +12,53 @@ function toggleMobileNavState() {
  */
 
 function initBurger() {
-  var burger = document.querySelector('.burger');
-  burger.addEventListener('click', toggleMobileNavState);
+  const burger = document.querySelector('.burger')
+  burger.addEventListener('click', toggleMobileNavState)
 }
 
-initBurger();
+initBurger()
+
+
+document.addEventListener('DOMContentLoaded',function(event){
+  // array with texts to type in typewriter
+  var dataText = document.querySelector("h1").innerHTML;
+  
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  function typeWriter(text, i, fnCallback) {
+    // check if text isn't finished yet
+    if (i < (text.length)) {
+      // add next character to h1
+     document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+
+      // wait for a while and call this function again for next character
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 20000);
+     }
+     // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+     typeWriter(dataText[i], 0, function(){
+       // after callback (and whole text has been animated), start next text
+       StartTextAnimation(i + 1);
+     });
+    }
+  }
+  // start the text animation
+  StartTextAnimation(0);
+});
+
