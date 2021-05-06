5238f8cf90ddb759c5fd01249d04a0e35ed57cfc
Dan Fabro <timpaxerror@gmail.com>
2021-05-07 00:32:15 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -63,38 +63,21 @@ document.addEventListener('DOMContentLoaded',function(event){
 });
 */
 
-// set up text to print, each item in array is new line
-var aText = new Array("Hi, I'm Dan.");
-var iSpeed = 100; // time delay of print out
-var iIndex = 0; // start printing array at this posision
-var iArrLength = aText[0].length; // the length of the text array
-var iScrollAt = 20; // start scrolling up at this many lines
+function typewrite (target, text, loop, speed) {
+  // (A) SET DEFAULT OPTIONS
+  target = document.getElementById(target);
+  if (speed === undefined) { speed = 200; }
+  if (loop === undefined) { loop = false; }
  
-var iTextPos = 0; // initialise text position
-var sContents = ''; // initialise contents variable
-var iRow; // initialise current row
- 
-function typewriter()
-{
- sContents =  ' ';
- iRow = Math.max(0, iIndex-iScrollAt);
- var destination = document.getElementById("typedtext");
- 
- while ( iRow < iIndex ) {
-  sContents += aText[iRow++] + '<br />';
- }
- destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "|";
- if ( iTextPos++ == iArrLength ) {
-  iTextPos = 0;
-  iIndex++;
-  if ( iIndex != aText.length ) {
-   iArrLength = aText[iIndex].length;
-   setTimeout("typewriter()", 500);
-  }
- } else {
-  setTimeout("typewriter()", iSpeed);
- }
+  // (B) DRAW TYPEWRITER
+  let pointer = 0;
+  let timer = setInterval(function(){
+    pointer++;
+    if (pointer <= text.length) {
+      target.innerHTML = text.substring(0, pointer);
+    } else {
+      if (loop) { pointer = 0; }
+      else { clearInterval(timer); }
+    }
+  }, speed);
 }
-
-
-typewriter();
